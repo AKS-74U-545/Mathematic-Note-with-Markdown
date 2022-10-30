ae91befdba09121091829810bf90e8b8b23c782e
Connor <1322078541@qq.com>
2022-10-30 13:45:59 +0800

    Connor-XiaoxinAir14-2020

--- gitPush.bat
+++ gitPush.bat
@@ -1,9 +1,4 @@
 git add .
 git commit -m "Connor-XiaoxinAir14-2020"
-
-git remote add origin git@gitee.com:connor-zou/Mathematic-Note-with-Markdown.git
-git push -u origin master
-
-git remote add origin git@github.com:avtoKalashnikov/Mathematic-Note-with-Markdown.git
 git push -u origin master
 
